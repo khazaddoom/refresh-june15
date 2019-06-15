d51089fe94ee773830696c908da58033ebe29eff
unknown <31121986g@gmail.com>
2019-06-15 14:04:41 +0530

    learning continued

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './components/server/server.component';
+import { ServersComponent } from './components/servers/servers.component';
+import { SuccessAlertComponent } from './components/success-alert/success-alert.component';
+import { WarningAlertComponent } from './components/warning-alert/warning-alert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    SuccessAlertComponent,
+    WarningAlertComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/warning-alert/warning-alert.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-warning-alert',
+  templateUrl: './warning-alert.component.html',
+  styleUrls: ['./warning-alert.component.css']
+})
+export class WarningAlertComponent implements OnInit {
+
+  message: string = `Maximum client refresh threshold of 100 has been reached.`;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
