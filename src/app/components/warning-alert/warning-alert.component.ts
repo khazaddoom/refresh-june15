69c11141eb59f5b2d7622be73216c2b14a509f59
unknown <31121986g@gmail.com>
2019-06-15 14:17:07 +0530

    learning continued

--- src/app/components/warning-alert/warning-alert.component.ts
+++ src/app/components/warning-alert/warning-alert.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class WarningAlertComponent implements OnInit {
 
   message: string = `Maximum client refresh threshold of 100 has been reached.`;
+  toggle = false;
 
   constructor() { }
 
